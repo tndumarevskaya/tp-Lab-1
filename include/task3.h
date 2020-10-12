f6b4c79d51261d5826a2471ac9cd83d6151413dd
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-10-12 18:46:16 +0300

    first commit

---
+++ include/task3.h
@@ -0,0 +1,6 @@
+#ifndef TASK3_H
+#define TASK3_H
+
+unsigned long long sumPrime(unsigned int hbound); //сумма всех чисел до hbound(не включая его)
+
+#endif // !TASK1_h
---
+++ include/task5.h
@@ -0,0 +1,7 @@
+#ifndef TASK5_H
+#define TASK5_H
+
+void split(char*** result, int* N, char* buf, char ch); //разбиение строки buf на подстроки и запись
+//результата в result, с присвоением по адресу N количества полученных подстрок.
+
+#endif // 
