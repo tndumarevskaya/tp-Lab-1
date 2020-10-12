bb6fdbaa955209772a444e9f9d32c6991aee2a56
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-10-12 18:46:57 +0300

    first commit

---
+++ task2.h
@@ -0,0 +1,8 @@
+#ifndef TASK2_H
+#define TASK2_H
+
+bool checkPrime(unsigned int value); //проверка числа на простоту.
+unsigned long long nPrime(unsigned n); //нахождение n - ого простого числа(в ряду).
+unsigned long long nextPrime(unsigned long long value); //нахождение ближайшего следующего простого числа к value.
+
+#endif // !TASK2_H
